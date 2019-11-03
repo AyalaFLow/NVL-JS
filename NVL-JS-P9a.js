fe033f0e58cf92c2d44d4e42999a9bc9572279ab
Guillermo Ayala <Guillermo.ayalacorral@gmail.com>
2019-11-04 00:23:34 +0100

    subimos a remoto practica realizada NVL-JS-P9a

---
+++ NVL-JS-P9a.js
@@ -0,0 +1,9 @@
+var lista = [[10, 20], [20, 40, 60, 80,"Edades"], [30, 60]];
+document.write("Longitud del ARRAY: " + " " + "<br>");
+document.write(lista.length + "<br>");
+document.write("Longitud del elemento 2 del ARRAY " + " " + "<br>");
+document.write(lista[1].length + "<br>");
+document.write("Valor del tercer elemento de la lista " + " " + "<br>");
+document.write(lista[2] + "<br>");
+document.write("Segundo elemento del tercer valor de la lista " + " " + "<br>");
+document.write(lista[2][1] + "<br>");
