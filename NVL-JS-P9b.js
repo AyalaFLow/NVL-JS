159b10ea7a584e7966dfd810d2708affbc5901a7
Guillermo Ayala <Guillermo.ayalacorral@gmail.com>
2019-11-04 03:59:01 +0100

    se sube a remoto NVL-JS-P9b realizada

---
+++ NVL-JS-P9b.js
@@ -0,0 +1,25 @@
+
+var conciertos = [
+	[" Lunes "," Madrid "], 
+	[" Martes "," Sevilla "],
+	[" Miercoles "," Barcelona "],
+	[" Jueves "," Valencia "],
+	[" Viernes "," Zaragoza "],
+	[" Sabado "," Bilbao "],
+	[" Domingo "," Donosti "]
+];
+
+for (var i=0;i<conciertos.length;i++){ 
+   	for (var j=0;j<conciertos[i].length;j++){ 
+      	document.write(conciertos[i][j]);
+   	} 
+} 
+
+
+
+
+
+
+
+
+
