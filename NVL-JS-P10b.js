3942e3c070f3d4f9e58876c73edf31d7c81aa78f
Guillermo Ayala <Guillermo.ayalacorral@gmail.com>
2019-11-04 19:53:23 +0100

    practica NVL-JS-P10a realizada

--- NVL-JS-P10b.js
+++ NVL-JS-P10b.js
@@ -1,2 +1,25 @@
-var azar =Math.floor((Math.random() * 100) + 1);
-document.write(azar);
+var pares = [];
+var impares = [];
+	document.write("50 primeros valores al azar entre 1-100: <br><br>");
+for (var i=0; i<50; i++){
+	var azar = Math.floor((Math.random()*100)+1);
+	document.write(azar + "<br>");
+	
+	if (azar %2 == 0){
+		pares.push(azar);
+	}
+	else{
+		impares.push(azar);
+		
+	}
+	
+}
+document.write("Valores pares: <br><br>");
+	document.write(pares + "<br>");	
+document.write("Valores impares: <br><br>");
+	document.write(impares + "<br>");	
+
+
+	
+		
+	
