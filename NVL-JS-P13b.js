03cc2998858185412a6d8ef8a4db400b1dd512a1
Guillermo Ayala <Guillermo.ayalacorral@gmail.com>
2019-11-05 17:38:20 +0100

    subimos archivo realizado a remoto

--- NVL-JS-P13b.js
+++ NVL-JS-P13b.js
@@ -4,7 +4,7 @@ var resultado;
 fraseUser = prompt("Ingresa la frase y te diremos si es Palindroma o no.");
 
 function comparacion(fraseUser){
-	fraseUser=fraseUser.toLowerCase().replace(/\s/g, "");
+	fraseUser=fraseUser.toLowerCase().replace(/\s/g,"");
 	vueltaFrase= fraseUser.split("").reverse().toString();
 	for (var i = 0;i<vueltaFrase.length; i++){
 		vueltaFrase = vueltaFrase.replace(",","");
@@ -17,4 +17,4 @@ function comparacion(fraseUser){
 	alert("Tu frase " + resultado);
 } 
 comparacion(fraseUser);
-alert(fraseUser.length);
+alert(vueltaFrase.length);
